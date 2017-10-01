a4adaa98d7d2188d469910559f863d2b148d3167
MrCosmo <mail@mrcosmo.info>
2017-10-01 17:55:18 +0200

    changed prefix string

--- de.jojoa.methods/stringMethods.cs
+++ de.jojoa.methods/stringMethods.cs
@@ -10,8 +10,8 @@ namespace RealifeGM.de.jojoa.methods
 {
     class stringMethods
     {
-        public static string console_prefix = "German Realife - ";
-        public static string main_prefix = "~g~German Realife ~w~- ";
+        public static string console_prefix = "GTA Universe - ";
+        public static string main_prefix = "~g~GTA Universe ~w~- ";
 
         public static string console_ressource_start = console_prefix + "Das Script wurde erfolgreich gestartet.";
         public static string console_login = console_prefix + "%name%, hat sich eingeloggt";
@@ -19,7 +19,7 @@ namespace RealifeGM.de.jojoa.methods
 
 
         public static string[] login_msg = { main_prefix + "Herzlich Willkommen, %name%.", main_prefix + "Wir konnten dich in unserer Datenbank finden!", main_prefix + "Bitte logge dich ein, indem du das Formular befolgst." };
-        public static string[] register_msg = { main_prefix + "Herzlich Willkommen, %name%.", main_prefix + "Wir konnten dich nicht in unserer Datenbank finden!", main_prefix + "Bitte registrire dich nun, indem du das Formular befolgst." };
+        public static string[] register_msg = { main_prefix + "Herzlich Willkommen, %name%.", main_prefix + "Wir konnten dich nicht in unserer Datenbank finden!", main_prefix + "Bitte registriere dich nun, indem du das Formular befolgst." };
 
         public static string server_success_getted_data = main_prefix + "Der Server hat erfolgreich die Daten empfangen.";
         public static string success_login = main_prefix + "Du hast dich erfolgreich eingeloggt.";
@@ -27,8 +27,5 @@ namespace RealifeGM.de.jojoa.methods
         public static string success_skin = main_prefix + "Du hast dir erfogreich einen Skin ausgesucht.";
 
         public static string[] choose_skin = {main_prefix + "Du hast noch keinen Skin gewählt.", main_prefix + "Bitte wähle nun einen aus"};
-
-
-
     }
 }
